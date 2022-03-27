c05888bb046e6953f2cff805d8553df284beded2
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2022-03-27 21:44:18 +0200

    init

---
+++ src/graph_library/cmake-build-debug/CMakeFiles/graph_library.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graph_library.dir/config_parser.cpp.o"
+  "CMakeFiles/graph_library.dir/main.cpp.o"
+  "graph_library"
+  "graph_library.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graph_library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
