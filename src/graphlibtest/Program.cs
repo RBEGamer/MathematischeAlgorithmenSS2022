818632f2be65aad239fcdc113ac0ce5d6cdada23
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2022-05-24 12:40:43 +0200

    .

--- src/graphlibtest/Program.cs
+++ src/graphlibtest/Program.cs
@@ -24,7 +24,17 @@ namespace graphlibtest
             flow_graph fg = new flow_graph();
             fg.load_from_file(Path.Combine(archiveFolder, "Fluss.txt"), true);
             fg.convert_costs_to_capacity();
-            double fg_res =  algorithms.edmonds_karp(fg, fg.get_node_with_id(0), fg.get_node_with_id(7)).MaxFlow;
+            System.Console.WriteLine("FLUSS 0 => 7 : " +algorithms.edmonds_karp(fg, fg.get_node_with_id(0), fg.get_node_with_id(7)).MaxFlow);
+
+            flow_graph fg2 = new flow_graph();
+            fg2.load_from_file(Path.Combine(archiveFolder, "Fluss2.txt"), true);
+            fg2.convert_costs_to_capacity();
+            System.Console.WriteLine("FLUSS 2 0 => 7 :" + algorithms.edmonds_karp(fg2, fg2.get_node_with_id(0), fg2.get_node_with_id(7)).MaxFlow);
+
+            flow_graph fg3 = new flow_graph();
+            fg3.load_from_file(Path.Combine(archiveFolder, "G_1_2.txt"), true);
+            fg3.convert_costs_to_capacity();
+            System.Console.WriteLine("FLUSS G_2_1 0 => 7 : " + algorithms.edmonds_karp(fg3, fg3.get_node_with_id(0), fg3.get_node_with_id(7)).MaxFlow);
 
             int b = 5;
 
